cc573f0e181e1d3db47607be390e9f1850d8d73d
ElyssaW <ejwinch1@gmail.com>
2021-01-07 02:09:25 -0600

    added some comments

--- main.js
+++ main.js
@@ -34,42 +34,71 @@ let particleSettings = {
 
 // constructor function for new Particles
 function Particle() {
+        // Sets the x/y value where the particle will be drawn
         this.x = particleSettings.startingX
         this.y = particleSettings.startingY
+        // Sets particle size
         this.size = particleSettings.particleSize
-        // Sets a different "ground Y" randomly for each particle, to simulate falling across a flat plane
-        //this.groundLevel = this.y + randomRange(-100, 300)
+        // Sets the point on the Y axix where the particle will "bounce," simulating the ground
         this.groundLevel = game.height
-        // Various settings, grabbed from the settings object
+        // Sets color
         this.color = particleSettings.color
+        // Velocity X and Velocity Y (or vx and vy) The particle will move on the x/y axis according to these
+        // values - they're how fast the particle is going in any given direction. 
         this.vx = randomRange(-5, 5)
         this.vy = randomRange(-5, -10)
         
         // Increment particle index and push particle to object-array
         particleIndex++
+        // Set particle index to current index
         particles[particleIndex] = this
         this.id = particleIndex
+        // Set life to 0
         this.life = 0
+        // Set maxlife to settings value
         this.maxLife = particleSettings.maxLife
     
         // Draw function
         this.draw = function() {
-            // Change x, y, vx, and vy
+            // Change x and y according to the vx and vy values
             this.x += this.vx
             this.y += this.vy
+            
             // Age the particle
             this.life++
 
-            // Bounce on ground
+            // Check to see if the particle has reached the "ground" boundary defined above
             if (this.y + particleSettings.particleSize > this.groundLevel) {
+                // This slows and reverses the particle's Y velocity, simulating a more realistic bounce motion
                 this.vy *= -0.6
                 this.vx *= 0.75
+                // Sets the particle back above the boundary. Since the Y velocity was reversed, the particle
+                // will move up and away from this boundary
                 this.y = this.groundLevel - particleSettings.particleSize
             }
 
-            // Alter vy based on gravity
+            // This is the meat of the particle movement: increment the Y velocity according to gravity
             this.vy += particleSettings.gravity
             
+            // This increases the particle's Y velocity based on the gravity value defined in settings
+            // If the particle is moving upwards, it's vy value will be negative, and gravity will
+            // slowly push it into the postive here. If it is already in the positive, gravity will increase it
+            // even further, making it look like it's gaining more and more speed. 
+            
+            // To demonstrate: if the particle has a y value of 100, a vy of -5, and a gravity value of .5, it will
+            // add the gravity value to its vy: -5 + .5 = -4.5
+            
+            // Then, it will add its vy to its y: 100 + -4.5 = 95.5. The particle moves upwards, but by less distance
+            // than before.
+            
+            // This repeats for every frame the particle is drawn on. The vy value will gain more and more "gravity,"
+            // causing the particle to rise less and less, until the vy flips into a positive value. Then, the particle
+            // will start to fall, and since gravity is still being added to it, it will fall more and more with
+            // each successive frame.
+            
+            // In short, gravity will make the particle rise slower, arc out of the rise into a fall, and then fall faster
+            
+            // This just draws a square to visually represent the particle. Nothin' fancy
             ctx.fillStyle = this.color
             ctx.fillRect(this.x, this.y, this.size, this.size)
 
